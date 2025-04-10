cde219da285028842a636dee3f7a1c95c7583816
matheusoliveira63 <matheus_oliveira.s@hotmail.com>
2025-04-10 18:52:12 -0300

    Invertendo valores de variaveis com operadores unarios

---
+++ operadores/src/OpUnarios.java
@@ -0,0 +1,13 @@
+public class OpUnarios {
+
+    public static void main(String[] args) {
+        
+        //invertendo valores de variaveis com operadoes unarios
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+    }
+}
