6620e450a6d8150c59a8c3f6b245a264fc17c0ab
matheusoliveira63 <matheus_oliveira.s@hotmail.com>
2025-04-10 19:05:39 -0300

    ++

--- operadores/src/OpUnarios.java
+++ operadores/src/OpUnarios.java
@@ -1,12 +1,19 @@
 public class OpUnarios {
 
     public static void main(String[] args) {
-        
+
         //invertendo valores de variaveis com operadoes unarios
 
         int numero = 5;
 
         numero = - numero;
+
+        System.out.println(numero);
+
+        //adicionando valor a variaveis 
+
+        numero++; // quando utilizado "++" eu adiciono +1 ao valor anterior, 
+                  //por exemplo, antes o valor era -5 e depos virou -4.
 
         System.out.println(numero);
     }
